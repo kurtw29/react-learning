4cad095c5ad4ca4f04c8827aa2c424c3763c1aa7
kurtw29 <ckurt37@gmail.com>
2019-01-10 18:51:42 -0800

    setup input form, axios api calls, render image list

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends React.Component {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                    // Authorization: 'Client-ID <KEY-ACCESS>'
+            }
+        });
+
+        this.setState( {images:response.data.results} );
+    }
+    render(){
+        return (
+            <div className="ui container" style={{ marginTop: '10px'}}>
+                <SearchBar onSubmitSearchBar={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
+            </div>
+        );
+    }
+}
 
 export default App;
---
+++ pics/src/components/ImageList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    const images = props.images.map( (image) => {
+        return (
+            // <div key={image.id}></div>   //put the "key" property in root tag if there's one
+            <img key={image.id} src={image.urls.regular} alt={image.description} />
+            );
+        })
+        //avoid referencing "image" repeatedly "image.id, image.urls, image.desc.." to clean up code format by destructuring the "image" object
+        //refactoring version:  
+        // const images = props.images.map( ({description, id, urls}) => {
+        //     return (
+        //         // <div key={image.id}></div>
+        //         <img key={id} src={urls.regular} alt={description} />
+        //     );
+        // })
+    return(
+        <div>{images}</div>
+    )
+};
+export default ImageList;
